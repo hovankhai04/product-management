0433c060ee48d4e0f6c8c14437c74c737aec0dc1
Van Khai <hovankhaipro1@gmail.com>
2026-06-06 22:18:15 +0700

    add login admin feature

---
+++ controllers/admin/auth.controller.js
@@ -0,0 +1,43 @@
+const Account = require('../../models/accounts.model');
+const systemConfig = require('../../configs/system');
+const md5 = require('md5');
+
+// [GET] /admin/auth/login
+module.exports.login = (req, res) => {
+  res.render('admin/pages/auth/login', {
+    pageTitle: 'Trang đăng nhập',
+  });
+};
+
+// [POSt] /admin/auth/login
+module.exports.loginPost = async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const user = await Account.findOne({
+    email: email,
+    deleted: false,
+  });
+
+  if (!user) {
+    req.flash('error', 'Email không tồn tại!');
+    res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
+    return;
+  }
+
+  if (user.password !== md5(password)) {
+    req.flash('error', 'Mật khẩu không đúng!');
+    res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
+    return;
+  }
+
+  if (user.status !== 'active') {
+    req.flash('error', 'Tài khoản đã bị khóa!');
+    res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
+    return;
+  }
+
+  res.cookie('token', user.token)
+
+  res.redirect(`${systemConfig.prefixAdmin}/dashboard`);
+};
---
+++ validates/admin/auth.validate.js
@@ -0,0 +1,16 @@
+const systemConfig = require('../../configs/system');
+
+module.exports.loginPost = async (req, res, next) => {
+  if (!req.body.email) {
+    req.flash('error', 'Vui lòng nhập email');
+    res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
+    return;
+  }
+
+  if (!req.body.password) {
+    req.flash('error', 'Vui lòng nhập mật khẩu');
+    res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
+    return;
+  }
+  next();
+};
