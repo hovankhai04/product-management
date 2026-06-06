29463ce1719e6300733bf400d346662b99b82bd7
Van Khai <hovankhaipro1@gmail.com>
2026-06-07 04:36:07 +0700

    add role-based authorization for admin routes

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -1,14 +1,21 @@
 const Account = require('../../models/accounts.model');
+const Role = require('../../models/role.model');
 const systemConfig = require('../../configs/system');
 
 module.exports.requireAuth = async (req, res, next) => {
   if (!req.cookies.token) {
     res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
   } else {
-    const user = await Account.findOne({ token: req.cookies.token });
+    const user = await Account.findOne({ token: req.cookies.token }).select(" -password ");
     if (!user) {
       res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
     } else {
+      const role = await Role.findOne({
+        _id: user.role_id
+      }).select(" title permissions ");
+
+      res.locals.role = role;
+      res.locals.user = user;
       next();
     }
   }
