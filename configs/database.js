adcd2fa92dcf95cfa6fe3d4b04bd9797c4f9789f
Van Khai <hovankhaipro1@gmail.com>
2026-06-08 08:10:50 +0700

    fix: add cached MongoDB connection to prevent timeout on Vercel

--- configs/database.js
+++ configs/database.js
@@ -1,10 +1,28 @@
 const mongoose = require('mongoose');
 
-module.exports.connect = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URL);
-    console.log("Connect Success!");
-  } catch (error) {
-    console.log("Connect Error!");
-  }
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
 }
+
+module.exports.connect = async () => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGO_URL, {
+      bufferCommands: false,
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+    console.log("Connect Success!");
+    return cached.conn;
+  } catch (error) {
+    console.log("Connect Error!", error);
+    throw error;
+  }
+};
