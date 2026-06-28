ba0f8a7887d273e71fd5b5a4b88e73acba58f922
Van Khai <hovankhaipro1@gmail.com>
2026-06-28 22:26:16 +0700

    add cancel friend request functionality

--- sockets/client/users.socket.js
+++ sockets/client/users.socket.js
@@ -3,6 +3,7 @@ const User = require('../../models/user.model');
 module.exports = (res) => {
 
   _io.once('connection', (socket) => {
+    // Chức năng gửi yêu cầu
     socket.on('CLIENT_ADD_FRIEND', async (userId) => {
       const myUserId = res.locals.user.id;
 
@@ -37,7 +38,43 @@ module.exports = (res) => {
           }
         })
       }
-    })
+    });
 
+    // Chức năng huỷ gửi yêu cầu
+    socket.on('CLIENT_CANCEL_FRIEND', async (userId) => {
+      const myUserId = res.locals.user.id;
+
+      // Xoá id của A vào acceptFriends của B
+      const existIdAinB = await User.findOne({
+        _id: userId,
+        acceptFriends: myUserId
+      });
+
+      if (existIdAinB) {
+        await User.updateOne({
+          _id: userId
+        }, {
+          $pull: {
+            acceptFriends: myUserId
+          }
+        })
+      }
+
+      // Xoá id của B vào requestFriends của A
+      const existIdBinA = await User.findOne({
+        _id: myUserId,
+        requestFriends: userId
+      });
+
+      if (existIdBinA) {
+        await User.updateOne({
+          _id: myUserId
+        }, {
+          $pull: {
+            requestFriends: userId
+          }
+        })
+      }
+    })
   });
 }
